1e4f1da8610bbea3e11e595cd5cd4dcc2d2a276b
Gerwin Vaatstra <gerwin.vaatstra@vodafoneziggo.com>
2024-04-30 15:07:18 +0200

    feat: fix working extra field

--- src/product/product.js
+++ src/product/product.js
@@ -1,10 +1,11 @@
 class Product {
-    constructor(id, type, name, version, color) {
+    constructor(id, type, name, version, color, extra) {
         this.id = id;
         this.type = type;
         this.name = name;
         this.version = version;
-        this.color = color
+        this.color = color;
+        this.extra = extra;
     }
 }
 
